f8f08a52703e1143017ba4b24a9f2a3ce7376b0d
chandan <chandan.singh@industrybuying.com>
2021-05-13 17:15:26 +0530

    login issuue and minor ui bug fixed

--- app/src/main/java/com/example/twittercloneapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/twittercloneapp/activity/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (TwitterCloneAppData.getUser() == null && authentication.currentUser == null) {
+        if (authentication.currentUser == null) {
             navigateToLoginActivity()
         } else {
             handleBottomNavigation()
