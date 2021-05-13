40b220b47a23311d12f89658e0ec6dbbd6d08b38
chandan <chandan.singh@industrybuying.com>
2021-05-13 14:16:55 +0530

    minor change in logic

--- app/src/main/java/com/example/twittercloneapp/adapters/TweetAdapter.kt
+++ app/src/main/java/com/example/twittercloneapp/adapters/TweetAdapter.kt
@@ -3,7 +3,6 @@ package com.example.twittercloneapp.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
@@ -11,7 +10,7 @@ import com.example.twittercloneapp.R
 import com.example.twittercloneapp.commons.TwitterCloneAppData
 import com.example.twittercloneapp.commons.autoNotify
 import com.example.twittercloneapp.model.Tweet
-import kotlinx.android.synthetic.main.fragment_profile.*
+import com.google.firebase.Timestamp
 import kotlinx.android.synthetic.main.row_layout_tweet.view.*
 import kotlin.properties.Delegates.observable
 
@@ -35,6 +34,7 @@ class TweetAdapter(private val listener: TweetAdapterListener): RecyclerView.Ada
     override fun onBindViewHolder(holder: TweetAdapter.ViewHolder, position: Int) {
         holder.tvTweet.text = tweets[position].tweetText
         holder.userName.text = tweets[position].userData?.name
+        holder.tvTime.text = tweets[position].timestamp?.let { getTimeString(it) }
         if (TwitterCloneAppData.getUser()?.id == tweets[position].userData?.id) {
             holder.btnUpdateTweet.visibility = View.VISIBLE
             holder.btnDeleteTweet.visibility = View.VISIBLE
@@ -43,12 +43,31 @@ class TweetAdapter(private val listener: TweetAdapterListener): RecyclerView.Ada
         holder.btnDeleteTweet.setOnClickListener { listener.onDeleteTweetTapped(tweets[position]) }
     }
 
+    private fun getTimeString(timeStamp: Timestamp) : String {
+        val tweetTime = timeStamp.seconds
+        val currentTime = System.currentTimeMillis()/1000
+        val diffSeconds = currentTime - tweetTime
+        return if (diffSeconds <= 60) {
+            "Just now"
+        } else if (diffSeconds in 61..3599) {
+            val minute = diffSeconds/60
+            "${minute}m ago"
+        } else if (diffSeconds in 3601..86399) {
+            val hour = diffSeconds/3600
+            "${hour}h ago"
+        } else {
+            val day = diffSeconds/86400
+            "${day}d ago"
+        }
+    }
+
 
     override fun getItemCount(): Int = tweets.size
 
     inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         var tvTweet: AppCompatTextView = v.tvTweet
         val userName: AppCompatTextView = v.tvName
+        val tvTime: AppCompatTextView = v.tvTime
         val btnUpdateTweet: AppCompatImageButton = v.btnUpdateTweet
         val btnDeleteTweet: AppCompatImageButton = v.btnDeleteTweet
     }
--- app/src/main/java/com/example/twittercloneapp/model/Tweet.kt
+++ app/src/main/java/com/example/twittercloneapp/model/Tweet.kt
@@ -1,7 +1,10 @@
 package com.example.twittercloneapp.model
 
+import com.google.firebase.Timestamp
+
 data class Tweet(
         var id: String? = null,
         val userData: UserData? = null,
-        val tweetText:  String? = null
+        val tweetText:  String? = null,
+        val timestamp: Timestamp?= null
 )
